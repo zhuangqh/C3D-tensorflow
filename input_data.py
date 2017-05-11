4cea3ad5fc7f115b4c7b1bb2410e4408a97c3082
zhuangqh <zhuangqhc@163.com>
2017-05-11 16:38:59 +0800

    fix bug: using float number as array index

--- input_data.py
+++ input_data.py
@@ -21,7 +21,7 @@ from __future__ import print_function
 import os
 from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
-import PIL.Image as Image
+from PIL import Image
 import random
 import numpy as np
 import cv2
@@ -73,17 +73,21 @@ def read_clip_and_label(filename, batch_size, start_pos=-1, num_frames_per_clip=
     if not shuffle:
       print("Loading a video clip from {}...".format(dirname))
     tmp_data, _ = get_frames_data(dirname, num_frames_per_clip)
-    img_datas = [];
+    img_datas = []
     if(len(tmp_data)!=0):
       for j in xrange(len(tmp_data)):
         img = Image.fromarray(tmp_data[j].astype(np.uint8))
-        if(img.width>img.height):
-          scale = float(crop_size)/float(img.height)
-          img = np.array(cv2.resize(np.array(img),(int(img.width * scale + 1), crop_size))).astype(np.float32)
+        width, height = img.size
+        if(width>height):
+          scale = float(crop_size)/float(height)
+          img = np.array(cv2.resize(np.array(img),(int(width * scale + 1), crop_size))).astype(np.float32)
         else:
-          scale = float(crop_size)/float(img.width)
-          img = np.array(cv2.resize(np.array(img),(crop_size, int(img.height * scale + 1)))).astype(np.float32)
-        img = img[(img.shape[0] - crop_size)/2:(img.shape[0] - crop_size)/2 + crop_size, (img.shape[1] - crop_size)/2:(img.shape[1] - crop_size)/2 + crop_size,:] - np_mean[j]
+          scale = float(crop_size)/float(width)
+          img = np.array(cv2.resize(np.array(img),(crop_size, int(height * scale + 1)))).astype(np.float32)
+        tmpl = int((img.shape[0] - crop_size)/2)
+        tmpr = int((img.shape[1] - crop_size)/2)
+        #img = img[(img.shape[0] - crop_size)/2:(img.shape[0] - crop_size)/2 + crop_size, (img.shape[1] - crop_size)/2:(img.shape[1] - crop_size)/2 + crop_size,:] - np_mean[j]
+        img = img[tmpl:tmpl+crop_size, tmpr:tmpr+crop_size, :] - np_mean[j]
         img_datas.append(img)
       data.append(img_datas)
       label.append(int(tmp_label))
